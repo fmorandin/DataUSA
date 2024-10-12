239fc73e80d9a96def1d95509fe98d568690eb04
Felipe Morandin <felipemorandin@gmail.com>
2024-10-12 10:21:11 +0100

    Initial Commit

---
+++ Data USA/Data_USAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Data_USAApp.swift
+//  Data USA
+//
+//  Created by Felipe Morandin on 12/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Data_USAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Data USAUITests/Data_USAUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Data_USAUITestsLaunchTests.swift
+//  Data USAUITests
+//
+//  Created by Felipe Morandin on 12/10/2024.
+//
+
+import XCTest
+
+final class Data_USAUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
